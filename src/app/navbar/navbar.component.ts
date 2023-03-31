22d1837a590c7c89a7232142facd4271a71f749f
Cristian Luca Tinkelemberg <lucatinkelemberg2000@gmail.com>
2023-03-31 20:03:08 -0300

    Adicion del formulario LogIn dentro de la NavBar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  loginForm = this.fb.group({
+    email: ['', [Validators.required,Validators.email]],
+    contraseña: ['', [Validators.required]]
+  });
+  constructor(private fb: FormBuilder) {
 
+  }
+    login() {
+          console.log(this.loginForm.value);
+          if(this.loginForm.valid) {
+              console.log("llamar al servicio");
+          } else {
+            alert("Error");
+          }
+    }
+    get email() { return this.loginForm.get('email'); }
+
+    get contraseña() { return this.loginForm.get('contraseña'); }
 }
