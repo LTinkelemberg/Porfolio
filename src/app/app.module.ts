beb8a035acb62dff22bf6e09f5520848ee9961c8
Cristian Luca Tinkelemberg <lucatinkelemberg2000@gmail.com>
2023-04-12 17:50:23 -0300

    Adicion de componentes contacto, reseña, foto y cv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ToolsComponent } from './tools/tools.component';
+import { FotoComponent } from './foto/foto.component';
+import { ReviewComponent } from './review/review.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { CvComponent } from './cv/cv.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavbarComponent,
-    ToolsComponent
+    ToolsComponent,
+    FotoComponent,
+    ReviewComponent,
+    EducacionComponent,
+    ContactoComponent,
+    CvComponent
   ],
   imports: [
     BrowserModule,
