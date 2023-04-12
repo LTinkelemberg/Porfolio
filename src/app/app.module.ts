cb45e5b27022f4007d05f67b5adc19fa43d799e5
Cristian Luca Tinkelemberg <lucatinkelemberg2000@gmail.com>
2023-04-12 10:18:08 -0300

    Adicion de segmento Herramientas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ToolsComponent } from './tools/tools.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    ToolsComponent
   ],
   imports: [
     BrowserModule,
